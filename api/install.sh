9c6b9e43e78b5b004b8b242da950dcf36757d3ea
Luremento <zahar2005zima@gmail.com>
2025-09-15 00:45:00 +0800

    i'm tired but i can do this

--- api/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Скачиваем и устанавливаем Composer
-curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
-php /tmp/composer-setup.php --install-dir=/tmp --filename=composer
-mv /tmp/composer /usr/local/bin/composer
-# Устанавливаем зависимости
-composer install --no-dev --optimize-autoloader
