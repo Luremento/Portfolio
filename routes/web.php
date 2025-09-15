5e111576b1b9781c46dbd85fecd0cbfe0a0cfbc8
Luremento <zahar2005zima@gmail.com>
2025-09-15 23:36:53 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,19 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-     return view('welcome');
-});
+     return view('home');
+})->name('home');
 
 Route::get('/about', function () {
      return view('about');
-});
+})->name('about');
 
 Route::get('/projects', function () {
      return view('projects');
-});
+})->name('projects');
 
 Route::get('/contact', function () {
      return view('contact');
-});
+})->name('contact');
 
 Route::post('/contact', [App\Http\Controllers\ContactController::class, 'store'])->name('contact.store');
