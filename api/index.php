924288cfb28517d8f1de76ec003d68e5b6d50296
Luremento <zahar2005zima@gmail.com>
2025-09-16 01:34:11 +0800

    update

--- api/index.php
+++ api/index.php
@@ -3,21 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-// define('LARAVEL_START', microtime(true));
+define('LARAVEL_START', microtime(true));
 
-// if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-//     require $maintenance;
-// }
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-// require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
-// $app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-// $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
-// $response->send();
-// $kernel->terminate($request, $response);
-
-require __DIR__ . "/../public/index.php";
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
