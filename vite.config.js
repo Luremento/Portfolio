c32e5807def5e77b0c1e8cbf1ddcf046b56030dd
Luremento <zahar2005zima@gmail.com>
2025-09-19 03:19:41 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/build/', // Указываем базовый путь для статических файлов
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
       build: {
         outDir: 'public/build',
-        manifest: true, // Убедимся, что манифест генерируется
-        assetsDir: 'assets', // Явно укажи директорию для активов
+        manifest: true,
+        assetsDir: 'assets',
       },
       refresh: true,
     }),
     tailwindcss(),
   ],
-  base: '/build/', // Базовый путь для статических файлов на Vercel
 });
