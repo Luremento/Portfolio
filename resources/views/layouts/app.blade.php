b49e9d40bdabea0b5c3330af7c94df35364f9c1d
Luremento <zahar2005zima@gmail.com>
2025-09-19 01:37:59 +0800

    Revert "update"
    This reverts commit bca0577a38aca9bedefa0bc36e225d04cca5e11f.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>molly - Fullstack Developer</title>
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&family=Open+Sans:wght@400;500;600&display=swap" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="{{ asset('css/globals.css') }}" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
             darkMode: 'class',
