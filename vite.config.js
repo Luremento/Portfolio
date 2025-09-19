210aed2feec8ca6efa4aa8241bad3f9cd74399b2
Luremento <zahar2005zima@gmail.com>
2025-09-19 22:52:16 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/build/', // Указываем базовый путь для статических файлов
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
